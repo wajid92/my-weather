35cf381626c2bfb7c320fb727a4dbb1cb540c802
Wajid <wajidmohdali786@gmail.com>
2023-03-06 19:31:23 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '24ae4c4447mshebd3259c107f1b0p1c229ejsn314d74459498',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '34c90ad94a531050c526a97f05fb644f';
