59585bc411ffd3a8ceda80d5b7c56eca497401c4
Wajid <wajidmohdali786@gmail.com>
2023-03-07 13:30:52 +0530

    .env added

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '24ae4c4447mshebd3259c107f1b0p1c229ejsn314d74459498',
+		'X-RapidAPI-Key': `${process.env.REACT_APP_RAPID_KEY}`,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -11,4 +11,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = '34c90ad94a531050c526a97f05fb644f';
+export const WEATHER_API_KEY = `${process.env.REACT_APP_OPENWEATHER_KEY}`;
